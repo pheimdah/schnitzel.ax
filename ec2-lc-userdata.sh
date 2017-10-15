bf488406c8a45d7550a2db54c424453fe5b3ed15
Patrik Heimdahl <pathei@gmail.com>
2017-10-15 14:01:41 +0300

    AWS EC2 LC user data improvements

--- ec2-lc-userdata.sh
+++ ec2-lc-userdata.sh
@@ -4,4 +4,9 @@ iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 8080
 rm -f /etc/localtime ; ln -s /usr/share/zoneinfo/Europe/Helsinki /etc/localtime
 yum --assumeyes update
 yum --assumeyes install java-1.8.0
-update-alternatives --install /usr/bin/java java /usr/lib/jvm/jre-1.8.0-openjdk.x86_64/bin/java 1100
+yum --assumeyes remove java-1.7.0-openjdk
+
+cd /home/ec2-user/
+sudo -u ec2-user wget https://s3.eu-central-1.amazonaws.com/schnitzel.ax-artifacts/schnitzelax/schnitzel.ax-1.0.jar -P /home/ec2-user/
+sudo -u ec2-user mkdir logs
+sudo -u ec2-user java -jar schnitzel.ax-1.0.jar > logs/server.log 2>&1 &
