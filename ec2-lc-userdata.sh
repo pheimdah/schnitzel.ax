ca1a755c9bf3aa310818c8c8a379a37a49062fca
Patrik Heimdahl <pathei@gmail.com>
2017-10-15 16:32:12 +0300

    Fetch code from GitHub and run gradle wrapper instead of jar

--- ec2-lc-userdata.sh
+++ ec2-lc-userdata.sh
@@ -3,10 +3,13 @@ set -ex
 iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 8080
 rm -f /etc/localtime ; ln -s /usr/share/zoneinfo/Europe/Helsinki /etc/localtime
 yum --assumeyes update
-yum --assumeyes install java-1.8.0
+yum --assumeyes install java-1.8.0-openjdk-devel
 yum --assumeyes remove java-1.7.0-openjdk
 
 cd /home/ec2-user/
-sudo -u ec2-user wget https://s3.eu-central-1.amazonaws.com/schnitzel.ax-artifacts/schnitzelax/schnitzel.ax-1.0.jar -P /home/ec2-user/
-sudo -u ec2-user mkdir logs
-sudo -u ec2-user java -jar schnitzel.ax-1.0.jar > logs/server.log 2>&1 &
+sudo -u ec2-user wget https://github.com/pheimdah/schnitzel.ax/archive/master.zip
+sudo -u ec2-user unzip master.zip
+sudo -u ec2-user rm master.zip
+sudo -u ec2-user cd schnitzel.ax-master/
+sudo -u ec2-user sh gradlew bootRun > logs/server.log 2>&1 &
+
