6fc2a83944a5cd80091068c043167477d329b9dd
Patrik Heimdahl <pathei@gmail.com>
2017-10-16 07:48:39 +0300

    Enabling scheduling of menu retrieval

--- src/main/java/ax/schnitzel/application/Application.java
+++ src/main/java/ax/schnitzel/application/Application.java
@@ -2,10 +2,9 @@ package ax.schnitzel.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
@@ -15,6 +14,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  */
 @ComponentScan(basePackages = { "ax.schnitzel" })
 @EnableAutoConfiguration
+@EnableScheduling
 public class Application extends WebMvcConfigurerAdapter {
 
 	/**
@@ -28,9 +28,9 @@ public class Application extends WebMvcConfigurerAdapter {
 	public void addResourceHandlers(final ResourceHandlerRegistry registry) {
 
 		// Add WebJars support
-		if (!registry.hasMappingForPattern("/webjars/**")) {
-			registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
-		}
+		// if (!registry.hasMappingForPattern("/webjars/**")) {
+		// registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+		// }
 	}
 
 }
