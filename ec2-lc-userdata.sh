e3fd7d8f80f35b3c1d53960d6db2ee44ebd253b5
Patrik Heimdahl <pathei@gmail.com>
2017-10-28 00:00:34 +0300

    1 GiB swap file for ec2 instances

--- ec2-lc-userdata.sh
+++ ec2-lc-userdata.sh
@@ -32,3 +32,9 @@ chown -R ec2-user:ec2-user /home/ec2-user/
 # Start schnitzel.ax web app
 cd schnitzel.ax-master/
 sudo -u ec2-user bash -c "sh gradlew bootRun > logs/server.log 2>&1 &"
+
+# Set up a 1 GiB swap file
+dd if=/dev/zero of=/swapfile bs=1024 count=1048576
+mkswap /swapfile
+chmod 0600 /swapfile
+swapon /swapfile
