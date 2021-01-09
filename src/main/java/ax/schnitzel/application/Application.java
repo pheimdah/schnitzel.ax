34be9dd7ab86b7ba4d119d9916380263b95a9a47
Patrik Heimdahl <pheimdah@users.noreply.github.com>
2021-01-09 18:17:25 +0200

    Version 1.2 - Going serverless & free of cost (#16)
    * Execute program in GitHub Actions
    
    * GitHub Actions file syntax fix
    
    * Cron syntax fix
    
    * Adding workflow_dispatch event trigger
    
    * Publish to GH Pages
    
    * Remove archiving
    
    Co-authored-by: Patrik Heimdahl <patrik@nypon.ax>

--- src/main/java/ax/schnitzel/application/Application.java
+++ src/main/java/ax/schnitzel/application/Application.java
@@ -1,10 +1,20 @@
 package ax.schnitzel.application;
 
+import java.io.FileWriter;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templateresolver.FileTemplateResolver;
+
+import ax.schnitzel.domain.service.SchnitzelService;
 
 /**
  * The application's entry point.
@@ -13,14 +23,38 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  */
 @ComponentScan(basePackages = { "ax.schnitzel" })
 @EnableAutoConfiguration
-@EnableScheduling
-public class Application extends WebMvcConfigurerAdapter {
+public class Application implements CommandLineRunner {
+
+	/** Injected dependency used for all schnitzel related operations */
+	@Autowired
+	SchnitzelService schnitzelService;
 
 	/**
 	 * @param args command-line arguments
 	 */
 	public static void main(final String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(Application.class, args).close();
 	}
 
+	public void run(String... arg0) throws Exception {
+
+		// Set up Thymeleaf
+		FileTemplateResolver templateResolver = new FileTemplateResolver();
+		templateResolver.setTemplateMode("HTML");
+		TemplateEngine templateEngine = new TemplateEngine();
+		templateEngine.setTemplateResolver(templateResolver);
+
+		// Fetch restaurants, store in Thymeleaf context
+		Context context = new Context();
+		context.setVariable("restaurants", schnitzelService.getRestaurants());
+
+		// Create 'output' dir
+		Files.createDirectories(Paths.get("output"));
+
+		// Generate today.html
+		Writer writer = new FileWriter("output/index.html");
+		writer.write(templateEngine.process("src/main/resources/templates/home.html", context));
+		writer.close();
+
+	}
 }
